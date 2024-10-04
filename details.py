82a787574fce8345bb8837191e250d07d9aeb10c
abdelrhman <abdelrhmanelkomey@gmail.com>
2024-10-04 16:45:16 +0300

    Edit all files

--- details.py
+++ details.py
@@ -1,5 +1,5 @@
 message = """
-Welcome Bro, Hello I Like Your Prjects
+Welcome Bro, Hello I Like Your Projects
 """
 
 bio = """
@@ -10,10 +10,6 @@ I have python programming language.
 Thanks and Goodbye.
 """
 
-readme = """
-
-"""
-
 name = """
 Abdelrhman Elkomey
 """
@@ -22,32 +18,8 @@ username = """
 @abdelrhmanelkomey
 """
 
-password = """
-1234567890
-"""
-
-number_phone = """
-+201234567890
-"""
-
-link_website = """
-https://abdelrhman.com/
-"""
-
 location = """
 egypt, cairo, nasr city
 """
 
-"""Accounts"""
-gmail = """abdelrhmanelkomey@gmail.com"""
-facebook = """Abd Elrhman El Eomey"""
-instgram = """@abdelrhmanelkomey2012"""
-twitter = """@abdelrhman917"""
-tiktik = """Abdelrhman Elkomey"""
-youtube = """Abdelrhman Elkomey"""
-github = """@Abdelrhman936"""
-
-
-accounts_social_media = ["gmail", "facebook", "instgram", "twitter", "tiktik", "youtube", "github"]
-
-details = ["message", "bio" , "readme", "name", "username", "password", "number_phone", 'link_website', 'location', *accounts_social_media]
+my_details = [message, bio, name, username, location]
