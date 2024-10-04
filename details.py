551ee07f092b8ae26874d2599c1b249f58f5a3d4
abdelrhman <abdelrhmanelkomey@gmail.com>
2024-10-04 16:13:39 +0300

    Add code in details.py file consists of my details

---
+++ details.py
@@ -0,0 +1,53 @@
+message = """
+Welcome Bro, Hello I Like Your Prjects
+"""
+
+bio = """
+My Name is Abdelrhman.
+I beginning in programming.
+I have 12 years.
+I have python programming language.
+Thanks and Goodbye.
+"""
+
+readme = """
+
+"""
+
+name = """
+Abdelrhman Elkomey
+"""
+
+username = """
+@abdelrhmanelkomey
+"""
+
+password = """
+1234567890
+"""
+
+number_phone = """
++201234567890
+"""
+
+link_website = """
+https://abdelrhman.com/
+"""
+
+location = """
+egypt, cairo, nasr city
+"""
+
+"""Accounts"""
+gmail = """abdelrhmanelkomey@gmail.com"""
+facebook = """Abd Elrhman El Eomey"""
+instgram = """@abdelrhmanelkomey2012"""
+twitter = """@abdelrhman917"""
+tiktik = """Abdelrhman Elkomey"""
+youtube = """Abdelrhman Elkomey"""
+github = """@Abdelrhman936"""
+
+
+accounts_social_media = [gmail, facebook, instgram, twitter, tiktik, youtube, github]
+
+details = [message, bio , readme, name, username, password, number_phone, link_website, location, *accounts_social_media]
