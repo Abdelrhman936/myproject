257ea5780138391498edf7d96d4b0c371966ce37
abdelrhman <abdelrhmanelkomey@gmail.com>
2024-10-04 17:45:39 +0300

    Edit all files

--- details.py
+++ details.py
@@ -1,25 +1,12 @@
-message = """
-Welcome Bro, Hello I Like Your Projects
-"""
-
-bio = """
-My Name is Abdelrhman.
-I beginning in programming.
-I have 12 years.
-I have python programming language.
+name = input("What's your name: ")
+age = int(input("What's your age: ").strip())
+username = input(f"{name}0{age}1").lower().strip()
+level = input("Choose yor level:\n[Beginner, Intermediate, Advanced, Expert, Master]\nWhat's your level: ")
+programming_language = input("What's programming language: ")
+bio = f"""
+My Name is {name}.
+I {level} in programming.
+I have {age} years.
+I have {programming_language} programming language.
 Thanks and Goodbye.
 """
-
-name = """
-Abdelrhman Elkomey
-"""
-
-username = """
-@abdelrhmanelkomey
-"""
-
-location = """
-egypt, cairo, nasr city
-"""
-
-my_details = [message, bio, name, username, location]
--- take_by_input.py
+++ take_by_input.py
@@ -1,2 +1,12 @@
 import details
-print(f"details : {details.my_details}")
+print(details.bio)
+take_by_input = input("\n\nEnter what do you want of details: ").lower().strip()
+
+if take_by_input == "name":
+    print(details.name)
+elif take_by_input == "age":
+    print(details.age)
+elif take_by_input == "username":
+    print(details.username)
+elif take_by_input == "programming_language":
+    print(details.programming_language)
